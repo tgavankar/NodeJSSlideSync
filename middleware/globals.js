79177d1ebe53c0c3a81985db4ceb1e1d48ad02f2
tgavankar <tgavankar@gmail.com>
2012-12-05 23:47:41 -0500

    present cleanup

--- middleware/globals.js
+++ middleware/globals.js
@@ -1,5 +1,6 @@
 module.exports = function globals(req, res, next) {
   res.locals.token = req.session._csrf;
   res.locals.user = req.user;
+  res.locals.base = "http://" + req.headers.host;
   next();
 }
